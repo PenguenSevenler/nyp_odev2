e679e63401d0cb4e12a8a7458af1dd63a49a86db
Yusuf Kozan <kozanyusuf@outlook.com.tr>
2023-11-04 16:55:03 +0300

    Faktöriyel sorusuna çözüm eklendi.

---
+++ soru5.37/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Faktöriyel
+{
+    internal class Program
+    {
+        internal static void Main(string[] args)
+        {
+            Console.Write("Faktöriyelini hesaplamak istediğiniz sayıyı girin: ");
+            try
+            {
+                uint n = uint.Parse(Console.ReadLine());
+                Console.WriteLine("Sonuç: {0}", Hesapla.Faktöriyel(n));
+            }
+            catch
+            {
+                Console.WriteLine("HATA: Bir doğal sayı girmeniz gerekiyor.");
+            }
+        }
+    }
+
+    internal class Hesapla
+    {
+        internal static long Faktöriyel(uint n)
+        {
+            if (n == 0)
+                return 1;
+            return n * Faktöriyel(n-1);
+        }
+    }
+}
