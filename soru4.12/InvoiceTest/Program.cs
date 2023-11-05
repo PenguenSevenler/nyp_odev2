950a4632114da7dbe4da640cb801d920e5cee965
Yusuf Kozan <kozanyusuf@outlook.com.tr>
2023-11-05 13:59:14 +0300

    Ürün sorgusu esnekleştirildi.

--- soru4.12/InvoiceTest/Program.cs
+++ soru4.12/InvoiceTest/Program.cs
@@ -18,7 +18,7 @@ internal class Invoice    // we created a class for Invoice elements
         this.Quantity = Quantity;
     }
 
-    protected internal decimal TotalPriceofItem() // we created a method to calculate the price for each item sold
+    protected internal decimal GetInvoiceAmount() // we created a method to calculate the price for each item sold
     {
         return this.PricePerItem * (decimal)this.Quantity;
     }
@@ -31,7 +31,7 @@ internal class Invoice    // we created a class for Invoice elements
     public override string ToString()
     {
         return $"{this.PartNumber} - {this.PartDescription} - {this.Quantity} pcs * {this.PricePerItem} USD" +
-                 $"= {this.TotalPriceofItem()} USD";
+                 $"= {this.GetInvoiceAmount()} USD";
     }
 }
 
@@ -52,11 +52,11 @@ public class Program
         products[8] = new Invoice("P009", "Middle Tool Kit", 21.40, 0);
         products[9] = new Invoice("P010", "Professional Tool Kit", 149.99, 0);
 
-        // ONCELIKLE URUN LISTEMIZI BASALIM
+        // We first print our product list
         Console.WriteLine("- - - OUR PRODUCT LIST - - -");
         Console.WriteLine("\n PN - NAME - PRICE");
 
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < products.Length; i++)
         {
             products[i].PrintItemInfo();
         }
@@ -66,11 +66,14 @@ public class Program
         string invoiceID = "invoice" + DateTime.Now.ToString("yyyyMMdd_HHmmss");
         string invoicePath = invoiceID + ".pdf";
 
-        int didBuy = 0;
+        bool bought = false;
 
         while (true)
         {
-            Console.Write("Do you want to buy anything ? (y/n) : ");
+            if (!bought)
+                Console.Write("Do you want to buy anything? (y/n) : ");
+            else
+                Console.Write("Do you want to buy another thing? (y/n) : ");
             string choice = Console.ReadLine();
 
             if (choice == "y" || choice == "Y")
@@ -79,169 +82,90 @@ public class Program
                 Console.Write("What do you want to buy? Only use PN : ");
                 string PN = Console.ReadLine();
 
-                switch (PN)  // PN PRODUCT NUMBER HANGI URUNU SECECEKSİN...
+                int index = -1; // Initial value is below zero to indicate an error state
+                for (int i = 0; i < products.Length; i++)
                 {
-                    case "P001":
-                        Console.Write("How many ALuminium Carabiners do you want? : ");
-                        products[0].Quantity += Convert.ToInt32(Console.ReadLine());
-                        if (products[0].Quantity > 0)
-                        {
-                            didBuy = 1;
-
-                        }
-                        break;
-
-                    case "P002":
-                        Console.Write("How many Stainless Steel Corner Brackets do you want? : ");
-                        products[1].Quantity += Convert.ToInt32(Console.ReadLine());
-                        if (products[1].Quantity > 0)
-                        {
-                            didBuy = 1;
-
-                        }
-                        break;
-
-                    case "P003":
-                        Console.Write("How many Work Gloves do you want? : ");
-                        products[2].Quantity += Convert.ToInt32(Console.ReadLine());
-                        if (products[2].Quantity > 0)
-                        {
-                            didBuy = 1;
-
-                        }
-                        break;
-
-                    case "P004":
-                        Console.Write("How many Plastic Chains do you want? : ");
-                        products[3].Quantity += Convert.ToInt32(Console.ReadLine());
-                        if (products[3].Quantity > 0)
-                        {
-                            didBuy = 1;
-
-                        }
-                        break;
-
-                    case "P005":
-                        Console.Write("How many Stainless Steel Springs do you want? : ");
-                        products[4].Quantity += Convert.ToInt32(Console.ReadLine());
-                        if (products[4].Quantity > 0)
-                        {
-                            didBuy = 1;
-
-                        }
-                        break;
-
-                    case "P006":
-                        Console.Write("How many PTFE Seal Thread Tape do you want? : ");
-                        products[5].Quantity += Convert.ToInt32(Console.ReadLine());
-                        if (products[5].Quantity > 0)
-                        {
-                            didBuy = 1;
-
-                        }
-                        break;
-
-                    case "P007":
-                        Console.Write("How many Silver Door Handles do you want? : ");
-                        products[6].Quantity += Convert.ToInt32(Console.ReadLine());
-                        if (products[6].Quantity > 0)
-                        {
-                            didBuy = 1;
-
-                        }
-                        break;
-
-                    case "P008":
-                        Console.Write("How many Mini Tool Kits do you want? : ");
-                        products[7].Quantity += Convert.ToInt32(Console.ReadLine());
-                        if (products[7].Quantity > 0)
-                        {
-                            didBuy = 1;
-
-                        }
-                        break;
-
-                    case "P009":
-                        Console.Write("How many Middle Tool Kits do you want? : ");
-                        products[8].Quantity += Convert.ToInt32(Console.ReadLine());
-                        if (products[8].Quantity > 0)
-                        {
-                            didBuy = 1;
-
-                        }
-                        break;
-
-                    case "P010":
-                        Console.Write("How many Professional Tool Kits do you want? : ");
-                        products[9].Quantity += Convert.ToInt32(Console.ReadLine());
-                        if (products[9].Quantity > 0)
-                        {
-                            didBuy = 1;
-
-                        }
-                        break;
-                    default:
-                        Console.WriteLine("Sorry, could not understand.");
-                        break;
-                }
-
-
-
-                if (didBuy == 1) // EGER SATIS YAPILDIYSA FATURA OLUSTURULACAK
-                { 
-                    iTextSharp.text.Document theReport = new iTextSharp.text.Document();
-                    PdfWriter.GetInstance(theReport, new FileStream(invoicePath, FileMode.Create));
-                    theReport.AddAuthor("Group 2");
-                    theReport.AddCreationDate();
-                    theReport.AddCreator("Group 2");
-                    theReport.AddSubject("Group 2 - Homework 2");
-
-                    if (theReport.IsOpen() == false)
-                        theReport.Open();
-
-
-                    iTextSharp.text.Paragraph headParagraph = new Paragraph("Hardware Store");
-
-                    headParagraph.Alignment = iTextSharp.text.Image.ALIGN_CENTER;
-               
-                    theReport.Add(new Paragraph(headParagraph));
-                    theReport.Add(new Paragraph(" "));
-
-                    theReport.Add(new Paragraph());
-
-
-                    // SADECE SATILAN URUNLER FATURADA OLACAK
-                    decimal total_price = 0;
-                    for (int i = 0; i < products.Length; i++)
+                    // We use linear search to determine the index of desired item
+                    if (products[i].PartNumber.Equals(PN))
                     {
-                        if (products[i].Quantity > 0)
-                        {
-                            theReport.Add(new Paragraph(products[i].ToString()));
-                            total_price += products[i].TotalPriceofItem();
-                        }
+                        index = i;
+                        break;
                     }
-                    
-                    iTextSharp.text.Paragraph theTotaL = new Paragraph("Total Price : " + total_price.ToString() + " USD");
-                    theTotaL.Alignment = iTextSharp.text.Image.ALIGN_RIGHT;
-                    theReport.Add(new Paragraph(" "));
-                    theReport.Add(new Paragraph(" "));
-                    theReport.Add(new Paragraph(theTotaL));
-                    
-                    theReport.Close();
                 }
 
+                if (index < 0)
+                {
+                    Console.WriteLine("No items found with given PN.");
+                }
+                else
+                {
+                    Console.Write("Enter the desired amount of {0}: ", products[index].PartDescription);
+                    try
+                    {
+                        products[index].Quantity += Convert.ToInt32(Console.ReadLine());
+                    }
+                    catch
+                    {
+                        Console.WriteLine("You must enter an integer.");
+                    }
+                    if (products[index].Quantity > 0)
+                    {
+                        bought = true;
+                    }
+                }
+                Console.Write("\n");
+                
             }
             else
             {
-                Console.WriteLine("\nSee You Again");
-
-                if (didBuy == 1)
-                    Console.WriteLine("Your invoice is here -> " + invoicePath);
-
-
                 break;
             }
         }
 
+        Console.Write("\nSee You Again\n");
+
+        if (bought) // An invoice file will be generated if anything is sold.
+        { 
+            iTextSharp.text.Document theReport = new iTextSharp.text.Document();
+            PdfWriter.GetInstance(theReport, new FileStream(invoicePath, FileMode.Create));
+            theReport.AddAuthor("Group 2");
+            theReport.AddCreationDate();
+            theReport.AddCreator("Group 2");
+            theReport.AddSubject("Group 2 - Homework 2");
+
+            if (theReport.IsOpen() == false)
+                theReport.Open();
+
+
+            iTextSharp.text.Paragraph headParagraph = new Paragraph("Hardware Store");
+
+            headParagraph.Alignment = iTextSharp.text.Image.ALIGN_CENTER;
+        
+            theReport.Add(new Paragraph(headParagraph));
+            theReport.Add(new Paragraph(" "));
+
+            theReport.Add(new Paragraph());
+
+
+            // Only the sold items will be on the invoice.
+            decimal total_price = 0;
+            for (int i = 0; i < products.Length; i++)
+            {
+                if (products[i].Quantity > 0)
+                {
+                    theReport.Add(new Paragraph(products[i].ToString()));
+                    total_price += products[i].GetInvoiceAmount();
+                }
+            }
+            
+            iTextSharp.text.Paragraph theTotaL = new Paragraph("Total Price : " + total_price.ToString() + " USD");
+            theTotaL.Alignment = iTextSharp.text.Image.ALIGN_RIGHT;
+            theReport.Add(new Paragraph(" "));
+            theReport.Add(new Paragraph(" "));
+            theReport.Add(new Paragraph(theTotaL));
+            
+            theReport.Close();
+            Console.WriteLine("Your invoice is here -> " + invoicePath);
+        }
     }
 }
