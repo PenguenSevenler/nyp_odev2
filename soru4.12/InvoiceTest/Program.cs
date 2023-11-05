3642ff0dffe7d0033362ea1fdc31cbd141f3efed
Yusuf Kozan <kozanyusuf@outlook.com.tr>
2023-11-05 10:33:08 +0300

    Ürünler dizi hâline getirildi.

--- soru4.12/InvoiceTest/Program.cs
+++ soru4.12/InvoiceTest/Program.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Drawing.Printing;
-using System.Drawing;
-using System.Text.RegularExpressions;
 using iTextSharp.text;
 using iTextSharp.text.pdf;
-using System.Reflection;
-using System.Globalization;
 
 internal class Invoice    // we created a class for Invoice elements
 {
@@ -23,7 +18,7 @@ internal class Invoice    // we created a class for Invoice elements
         this.Quantity = Quantity;
     }
 
-    protected internal decimal PriceOfTotalItem() // we created a method to calculate the price for each item sold
+    protected internal decimal TotalPriceofItem() // we created a method to calculate the price for each item sold
     {
         return this.PricePerItem * (decimal)this.Quantity;
     }
@@ -36,7 +31,7 @@ internal class Invoice    // we created a class for Invoice elements
     public override string ToString()
     {
         return $"{this.PartNumber} - {this.PartDescription} - {this.Quantity} pcs * {this.PricePerItem} USD" +
-                 $"= {this.PriceOfTotalItem()} USD";
+                 $"= {this.TotalPriceofItem()} USD";
     }
 }
 
@@ -45,43 +40,31 @@ public class Program
 {
     public static void Main()
     {
-        Invoice product001 = new Invoice("P001", "Aluminium Carabiner", 0.40, 0);
-        Invoice product002 = new Invoice("P002", "Stainless Steel Corner Bracket", 0.12, 0);
-        Invoice product003 = new Invoice("P003", "Work Gloves", 0.45, 0);
-        Invoice product004 = new Invoice("P004", "Plastic Chain", 0.20, 0);
-        Invoice product005 = new Invoice("P005", "Stainless Steel Spring", 0.30, 0);
-        Invoice product006 = new Invoice("P006", "PTFE Seal Thread Tape", 0.16, 0);
-        Invoice product007 = new Invoice("P007", "Silver Door Handle", 0.21, 0);
-        Invoice product008 = new Invoice("P008", "Mini Tool Kit", 3.25, 0);
-        Invoice product009 = new Invoice("P009", "Middle Tool Kit", 21.40, 0);
-        Invoice product010 = new Invoice("P010", "Professional Tool Kit", 149.99, 0);
-
-        Console.WriteLine("- - - OUR PRODUCT LIST - - -\n");
-        Console.WriteLine("\n PN - NAME - PRICE");
+        Invoice[] products = new Invoice[10];
+        products[0] = new Invoice("P001", "Aluminium Carabiner", 0.40, 0);
+        products[1] = new Invoice("P002", "Stainless Steel Corner Bracket", 0.12, 0);
+        products[2] = new Invoice("P003", "Work Gloves", 0.45, 0);
+        products[3] = new Invoice("P004", "Plastic Chain", 0.20, 0);
+        products[4] = new Invoice("P005", "Stainless Steel Spring", 0.30, 0);
+        products[5] = new Invoice("P006", "PTFE Seal Thread Tape", 0.16, 0);
+        products[6] = new Invoice("P007", "Silver Door Handle", 0.21, 0);
+        products[7] = new Invoice("P008", "Mini Tool Kit", 3.25, 0);
+        products[8] = new Invoice("P009", "Middle Tool Kit", 21.40, 0);
+        products[9] = new Invoice("P010", "Professional Tool Kit", 149.99, 0);
 
         // ONCELIKLE URUN LISTEMIZI BASALIM
+        Console.WriteLine("- - - OUR PRODUCT LIST - - -");
+        Console.WriteLine("\n PN - NAME - PRICE");
 
-
-        product001.PrintItemInfo();
-        product002.PrintItemInfo();
-        product003.PrintItemInfo();
-        product004.PrintItemInfo();
-        product005.PrintItemInfo();
-        product006.PrintItemInfo();
-        product007.PrintItemInfo();
-        product008.PrintItemInfo();
-        product009.PrintItemInfo();
-        product010.PrintItemInfo();
-
+        for (int i = 0; i < 10; i++)
+        {
+            products[i].PrintItemInfo();
+        }
         Console.WriteLine("\n");
 
-        // we created a string to create a new file name.
-
-
+        // We create a new file name for invoice PDF
         string invoiceID = "invoice" + DateTime.Now.ToString("yyyyMMdd_HHmmss");
-
         string invoicePath = invoiceID + ".pdf";
-
 
         int didBuy = 0;
 
@@ -96,13 +79,12 @@ public class Program
                 Console.Write("What do you want to buy? Only use PN : ");
                 string PN = Console.ReadLine();
 
-
                 switch (PN)  // PN PRODUCT NUMBER HANGI URUNU SECECEKSİN...
                 {
                     case "P001":
                         Console.Write("How many ALuminium Carabiners do you want? : ");
-                        product001.Quantity += Convert.ToInt32(Console.ReadLine());
-                        if (product001.Quantity > 0)
+                        products[0].Quantity += Convert.ToInt32(Console.ReadLine());
+                        if (products[0].Quantity > 0)
                         {
                             didBuy = 1;
 
@@ -111,8 +93,8 @@ public class Program
 
                     case "P002":
                         Console.Write("How many Stainless Steel Corner Brackets do you want? : ");
-                        product002.Quantity += Convert.ToInt32(Console.ReadLine());
-                        if (product002.Quantity > 0)
+                        products[1].Quantity += Convert.ToInt32(Console.ReadLine());
+                        if (products[1].Quantity > 0)
                         {
                             didBuy = 1;
 
@@ -121,8 +103,8 @@ public class Program
 
                     case "P003":
                         Console.Write("How many Work Gloves do you want? : ");
-                        product003.Quantity += Convert.ToInt32(Console.ReadLine());
-                        if (product003.Quantity > 0)
+                        products[2].Quantity += Convert.ToInt32(Console.ReadLine());
+                        if (products[2].Quantity > 0)
                         {
                             didBuy = 1;
 
@@ -131,8 +113,8 @@ public class Program
 
                     case "P004":
                         Console.Write("How many Plastic Chains do you want? : ");
-                        product004.Quantity += Convert.ToInt32(Console.ReadLine());
-                        if (product004.Quantity > 0)
+                        products[3].Quantity += Convert.ToInt32(Console.ReadLine());
+                        if (products[3].Quantity > 0)
                         {
                             didBuy = 1;
 
@@ -141,8 +123,8 @@ public class Program
 
                     case "P005":
                         Console.Write("How many Stainless Steel Springs do you want? : ");
-                        product005.Quantity += Convert.ToInt32(Console.ReadLine());
-                        if (product005.Quantity > 0)
+                        products[4].Quantity += Convert.ToInt32(Console.ReadLine());
+                        if (products[4].Quantity > 0)
                         {
                             didBuy = 1;
 
@@ -151,8 +133,8 @@ public class Program
 
                     case "P006":
                         Console.Write("How many PTFE Seal Thread Tape do you want? : ");
-                        product006.Quantity += Convert.ToInt32(Console.ReadLine());
-                        if (product006.Quantity > 0)
+                        products[5].Quantity += Convert.ToInt32(Console.ReadLine());
+                        if (products[5].Quantity > 0)
                         {
                             didBuy = 1;
 
@@ -161,8 +143,8 @@ public class Program
 
                     case "P007":
                         Console.Write("How many Silver Door Handles do you want? : ");
-                        product007.Quantity += Convert.ToInt32(Console.ReadLine());
-                        if (product007.Quantity > 0)
+                        products[6].Quantity += Convert.ToInt32(Console.ReadLine());
+                        if (products[6].Quantity > 0)
                         {
                             didBuy = 1;
 
@@ -171,8 +153,8 @@ public class Program
 
                     case "P008":
                         Console.Write("How many Mini Tool Kits do you want? : ");
-                        product008.Quantity += Convert.ToInt32(Console.ReadLine());
-                        if (product008.Quantity > 0)
+                        products[7].Quantity += Convert.ToInt32(Console.ReadLine());
+                        if (products[7].Quantity > 0)
                         {
                             didBuy = 1;
 
@@ -181,8 +163,8 @@ public class Program
 
                     case "P009":
                         Console.Write("How many Middle Tool Kits do you want? : ");
-                        product009.Quantity += Convert.ToInt32(Console.ReadLine());
-                        if (product009.Quantity > 0)
+                        products[8].Quantity += Convert.ToInt32(Console.ReadLine());
+                        if (products[8].Quantity > 0)
                         {
                             didBuy = 1;
 
@@ -191,8 +173,8 @@ public class Program
 
                     case "P010":
                         Console.Write("How many Professional Tool Kits do you want? : ");
-                        product010.Quantity += Convert.ToInt32(Console.ReadLine());
-                        if (product010.Quantity > 0)
+                        products[9].Quantity += Convert.ToInt32(Console.ReadLine());
+                        if (products[9].Quantity > 0)
                         {
                             didBuy = 1;
 
@@ -218,7 +200,7 @@ public class Program
                         theReport.Open();
 
 
-                    iTextSharp.text.Paragraph headParagraph = new Paragraph("Penguin Lovers Hardware");
+                    iTextSharp.text.Paragraph headParagraph = new Paragraph("Hardware Store");
 
                     headParagraph.Alignment = iTextSharp.text.Image.ALIGN_CENTER;
                
@@ -228,87 +210,24 @@ public class Program
                     theReport.Add(new Paragraph());
 
 
-
                     // SADECE SATILAN URUNLER FATURADA OLACAK
-
-
-                    if (product001.Quantity > 0)
+                    decimal total_price = 0;
+                    for (int i = 0; i < products.Length; i++)
                     {
-                        theReport.Add(new Paragraph(product001.ToString()));
-                    }
-
-                    if (product002.Quantity > 0)
-                    {
-                        theReport.Add(new Paragraph(product002.ToString()));
-                    }
-
-                    if (product003.Quantity > 0)
-                    {
-                        theReport.Add(new Paragraph(product003.ToString()));
-                    }
-
-                    if (product004.Quantity > 0)
-                    {
-                        theReport.Add(new Paragraph(product004.ToString()));
-                    }
-
-                    if (product005.Quantity > 0)
-                    {
-                        theReport.Add(new Paragraph(product005.ToString()));
-                    }
-
-                    if (product006.Quantity > 0)
-                    {
-                        theReport.Add(new Paragraph(product006.ToString()));
-                    }
-
-                    if (product007.Quantity > 0)
-                    {
-                        theReport.Add(new Paragraph(product007.ToString()));
+                        if (products[i].Quantity > 0)
+                        {
+                            theReport.Add(new Paragraph(products[i].ToString()));
+                            total_price += products[i].TotalPriceofItem();
+                        }
                     }
                     
-                    if (product008.Quantity > 0)
-                    {
-                        theReport.Add(new Paragraph(product008.ToString()));
-                    }
-
-                    if (product009.Quantity > 0)
-                    {
-                        theReport.Add(new Paragraph(product009.ToString()));
-                    }
-
-                    if (product010.Quantity > 0) 
-                    {
-                        theReport.Add(new Paragraph(product010.ToString()));
-                    }
-
-
-
-
-             decimal InvoiceTotal = product001.PriceOfTotalItem();
-                    InvoiceTotal += product002.PriceOfTotalItem();
-                    InvoiceTotal += product003.PriceOfTotalItem();
-                    InvoiceTotal += product004.PriceOfTotalItem();
-                    InvoiceTotal += product005.PriceOfTotalItem();
-                    InvoiceTotal += product006.PriceOfTotalItem();
-                    InvoiceTotal += product007.PriceOfTotalItem();
-                    InvoiceTotal += product008.PriceOfTotalItem();
-                    InvoiceTotal += product009.PriceOfTotalItem();
-                    InvoiceTotal += product010.PriceOfTotalItem();
-
-                    InvoiceTotal = Math.Round(InvoiceTotal, 6);
-
-                    iTextSharp.text.Paragraph theTotaL = new Paragraph("Total Price : " + InvoiceTotal.ToString() + " USD");
+                    iTextSharp.text.Paragraph theTotaL = new Paragraph("Total Price : " + total_price.ToString() + " USD");
                     theTotaL.Alignment = iTextSharp.text.Image.ALIGN_RIGHT;
                     theReport.Add(new Paragraph(" "));
                     theReport.Add(new Paragraph(" "));
                     theReport.Add(new Paragraph(theTotaL));
                     
-
-
-
-                    theReport.Close();                
-
+                    theReport.Close();
                 }
 
             }
