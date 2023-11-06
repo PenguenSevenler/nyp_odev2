7c2cb9bfd19dd4569cbd355f0265a6ac3bb312f4
Yusuf Kozan <kozanyusuf@outlook.com.tr>
2023-11-06 18:10:56 +0300

    5.37 ile 5.38'in çözümleri birleştirildi.

--- soru5.37-38/Program.cs
+++ soru5.37-38/Program.cs
@@ -1,13 +1,37 @@
 ﻿using System;
 
-namespace e_number
+namespace Calculator
 {
     internal class Program
     {
         internal static void Main(string[] args)
         {
+            factorial_prompt();
+            Console.WriteLine("\n---\n");
+            e_prompt();
+        }
+
+        internal static void factorial_prompt()
+        {
+            Console.Write("Enter a number to calculate the factorial of: ");
+            try
+            {
+                uint number = 0;
+                number = uint.Parse(Console.ReadLine());
+
+                Console.Write("\nResult: {0}\n", Operation.Factorial(number));
+            }
+            catch
+            {
+                Console.Write("\nYou must enter a natural number.\n");
+                return;
+            }
+        }
+
+        internal static void e_prompt()
+        {
             uint approximation = 0;
-            Console.Write("Enter the number of steps to be performed: ");
+            Console.Write("Enter the number of steps to be performed to calculate e: ");
             try
             {
                 approximation = Convert.ToUInt32(Console.ReadLine());
@@ -21,17 +45,21 @@ namespace e_number
 
             try
             {
-                decimal calculation = e_number(approximation);
+                decimal calculation = Operation.E_Number(approximation);
                 Console.WriteLine("Our calculation of e:\t{0}", calculation);
                 Console.WriteLine("Math library's e:\t{0}", Math.E);
-                Console.WriteLine("\nRelative error:\t\t{0}", RelativeError(calculation, (decimal)Math.E));
+                Console.WriteLine("\nRelative error:\t\t{0}",
+                                    Operation.RelativeError(calculation, (decimal)Math.E));
             }
             catch
             {
                 Console.WriteLine("Calculation failed.");
             }
         }
+    }
 
+    internal class Operation
+    {
         internal static long Factorial(uint number)
         {
             if (number <= 1)
@@ -40,7 +68,7 @@ namespace e_number
                 return number * Factorial(number - 1);
         }
 
-        internal static decimal e_number(uint step)
+        internal static decimal E_Number(uint step)
         {
             decimal eNumber = 0;
 
