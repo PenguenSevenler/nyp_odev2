2b3ac8cfd1c4c148c17a843943a34da007b47393
gunesbalci <117308245+gunesbalci@users.noreply.github.com>
2023-11-06 11:19:50 +0300

    Add files via upload

---
+++ nyp5.37/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Program
+{
+    internal int fact(int n)//faktoriyel fonksiyonu
+    {
+        int sonuc=1;
+        for(int i=n;i>0;i--)
+        {
+            sonuc=sonuc*i;
+        }
+        return sonuc;
+    }
+    
+    public static void Main(string[] args)
+    {
+        int n;
+        double e=0;
+        Program f= new Program();
+        Console.WriteLine("step number of calculating e: ");
+        n=Convert.ToInt32(Console.ReadLine());
+        for(int i=0;i<n;i++) //e yi hesaplayan for dongusu
+        {
+            e=e+(1/(double)f.fact(i));
+        }
+        Console.WriteLine($"My e with {n} step: {e}\nMath libs e: {Math.E}");
+    }
+}
